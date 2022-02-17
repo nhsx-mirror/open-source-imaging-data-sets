f7021a7442dd48e1289e6a530001263afbb6bb7e
Amadeus Stevenson <amadeus.stevenson@nhsx.nhs.uk>
2022-02-17 10:43:33 +0000

    Refactor code into function

--- utils/screenshot.py
+++ utils/screenshot.py
@@ -5,37 +5,56 @@ from webdriver_manager.chrome import ChromeDriverManager
 from PIL import Image
 from io import BytesIO
 
+# Define settings
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-# chrome_options.add_argument(f"--force-device-scale-factor=1.0")
 
 screenshot_path = "../public/screenshots/"
 
+# Create a new headlesss browser
 driver = webdriver.Chrome(
     service=Service(ChromeDriverManager().install()), options=chrome_options
 )
 
+# Set the size of the browser
 screenshot_width = 1280
 screenshot_height = 1024
-thumbnail_width = 300
-thumbnail_height = (int)(thumbnail_width * (screenshot_height / screenshot_width))
 
 driver.set_window_size(screenshot_width, screenshot_height)
 
-url = "https://www.cancerimagingarchive.net/collections/"
-try:
-    # grab the screenshot
-    driver.get(url)
-    screen = driver.get_screenshot_as_png()
+# Define function to grab, resize and save screenshot
+def grab_screenshot(
+    url, screenshot, driver=driver, thumbnail_width=300, thumbnail_height=240
+):
+    """Saves a screenshot of a URL
 
-    # resize the screenshot
-    img = Image.open(BytesIO(screen))
-    img = img.resize((thumbnail_width, thumbnail_height), Image.ANTIALIAS)
+    Args:
+        url (string): URL of website to grab
+        screenshot (string): Path and filename of screenshot to save
+        driver (object): webdriver object
+        thumbnail_width (int): width of thumbnail (px)
+        thumbnail_height (int): height of thumbnail (px)
+    """
 
-    # save the screenshot
-    img.save(screenshot_path + "screenshot.png")
-    # driver.save_screenshot(screenshot_path + "screenshot.png")
-except:
-    raise Exception(f"Error snapshotting {url}")
+    try:
+        # grab the screenshot
+        driver.get(url)
+        screen = driver.get_screenshot_as_png()
+
+        # resize the screenshot
+        img = Image.open(BytesIO(screen))
+        img = img.resize((thumbnail_width, thumbnail_height), Image.ANTIALIAS)
+
+        # save the screenshot
+        img.save(screenshot)
+        # driver.save_screenshot(screenshot_path + "screenshot.png")
+    except:
+        raise Exception(f"Error snapshotting {url}")
+
+
+grab_screenshot(
+    url="https://stackoverflow.com/questions/53657215/running-selenium-with-headless-chrome-webdriver",
+    screenshot=f"{screenshot_path}test.png",
+)
 
 driver.close()
